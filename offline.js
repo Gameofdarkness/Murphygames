38675e2c90f2dd25674323cfec6e46bc30038950
diyar-2020 <dyartymwry1@gmail.com>
2020-05-02 15:56:49 +0430

    new game

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1588418699,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"logo-sheet0.png",
+		"logo_bg.png",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
